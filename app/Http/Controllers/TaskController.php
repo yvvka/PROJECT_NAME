399636ce61d01861814f3d8bb714ef2ab459c3c6
yvvka <rhhf0006@yahoo.co.jp>
2023-07-13 17:20:39 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+{
+    $folders = Folder::all();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $id,
+    ]);
+}
+}
---
+++ database/seeders/FoldersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoldersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
